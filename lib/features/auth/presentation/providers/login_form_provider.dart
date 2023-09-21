ad246183ca08c50210db7aefb26c8e33fedbb596
Helmuth Sodetz <sodhel2001@gmail.com>
2023-09-21 15:43:26 -0500

    loginFormNotifier y loginFormProvider

--- lib/features/auth/presentation/providers/login_form_provider.dart
+++ lib/features/auth/presentation/providers/login_form_provider.dart
@@ -1,9 +1,10 @@
 
-
-//! 1. State del provider
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:formz/formz.dart';
 import 'package:teslo_shop/features/shared/shared.dart';
 
+
+//! 1. State del provider
 class LoginFormState {
   final bool isPosting;
   final bool isFormPosted;
@@ -15,18 +16,94 @@ class LoginFormState {
     this.isFormPosted = false, 
     this.isPosting = false, 
     this.isValid = false, 
-    this.email = const Email.pure(), 
+    this.email = const Email.pure(),
     this.password = const Password.pure()
   });
 
-  
+  LoginFormState copyWith({
+    bool? isPosting,
+    bool? isFormPosted,
+    bool? isValid,
+    Email? email,
+    Password? password,
+  }) => LoginFormState(
+    isPosting: isPosting ?? this.isPosting,
+    isFormPosted: isFormPosted ?? this.isFormPosted,
+    isValid: isValid ?? this.isValid,
+    email: email ?? this.email,
+    password: password ?? this.password
+  );
+
+  @override
+  String toString() {
+    return '''
+      isPosting: $isPosting
+      isFormPosted: $isFormPosted
+      isValid: $isValid
+      email: $email
+      password: $password
+
+    ''';
+  }
+
+
 
 
 }
 
 //! 2. Como implementamos un notifier
 
+class LoginFormNotifier extends StateNotifier<LoginFormState> {
+  LoginFormNotifier(): super(LoginFormState());
+
+  onEmailChange(String value) {
+    final newEmail = Email.dirty(value);
+    state = state.copyWith(
+      email: newEmail, 
+      isValid: Formz.validate([newEmail, state.password]),
+
+    );
+  }
+
+  onPasswordCahnge(String value) {
+    final newPassword = Password.dirty(value);
+    state = state.copyWith(
+      password: newPassword, 
+      isValid: Formz.validate([newPassword, state.email]),
+
+    );
+  }
+
+  onFormSubmit() {
+    _touchEveryField();
+
+    if (!state.isValid) return;
+
+    print('$state');
+
+  }
+
+  _touchEveryField() {
+    final email = Email.dirty(state.email.value);
+    final password = Password.dirty(state.password.value);
+
+    // se hace el cambio de estado para notificar a todos sus listeners
+    state = state.copyWith(
+      isFormPosted: true,
+      email: email,
+      password: password,
+      isValid: Formz.validate([email, password]),
+    );
+  }
+  
+}
+
 //! 3. StateNotifierProvider -> que es el que se consume afuera
+
+// El autodispose se usa para destruir lo que esta almacenado en los camppos del login, por si ya se ha logueado y vuelve no le aparezca sus credenciales
+final loginFormProvider = StateNotifierProvider.autoDispose<LoginFormNotifier, LoginFormState>((ref) {
+  return LoginFormNotifier();
+});
 
 
 
