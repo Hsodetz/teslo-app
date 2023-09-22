71e588314b923fce69c1dadf879315312205a201
Helmuth Sodetz <sodhel2001@gmail.com>
2023-09-22 10:07:31 -0500

    implementacion de AuthDatasoruce

--- lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
+++ lib/features/auth/infrastructure/datasources/auth_datasource_impl.dart
@@ -1,16 +1,41 @@
 
 
+import 'package:dio/dio.dart';
+import 'package:teslo_shop/config/constants/environment.dart';
 import 'package:teslo_shop/features/auth/domain/domain.dart';
+import 'package:teslo_shop/features/auth/infrastructure/errors/auth_errors.dart';
+import 'package:teslo_shop/features/auth/infrastructure/mappers/user_mapper.dart';
 
 class AuthDataSourceImpl implements AuthDataSource {
+
+  final dio = Dio(
+    BaseOptions(
+      baseUrl: Environment.apiUrl,
+    ),
+  );
+
   @override
   Future<User> checkAuthStatus(String token) {
     throw UnimplementedError();
   }
 
   @override
-  Future<User> login(String email, String password) {
-    throw UnimplementedError();
+  Future<User> login(String email, String password) async{
+
+    try {
+      final response = await dio.post('/auth/api', data: {
+        'email': email,
+        'password': password,
+      });
+
+      final user = UserMapper.userJsonToEntity(response.data);
+
+      return user;
+      
+    } catch (e) {
+      throw WrongCredentials();
+    }
+
   }
 
   @override
---
+++ lib/features/auth/infrastructure/errors/auth_errors.dart
@@ -0,0 +1,4 @@
+
+class InvalidToken implements Exception {}
+class WrongCredentials implements Exception {}
+
