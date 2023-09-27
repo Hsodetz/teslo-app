f1df4d95101c62ccc71e2a8f7099221f8c217ea5
Helmuth Sodetz <sodhel2001@gmail.com>
2023-09-27 10:13:01 -0500

    patron adaptador - servicio con los metodos para tomar una foto de la camara y/o obtener foto de la galeria.

---
+++ lib/features/shared/infrastructure/services/camera_gallery_service_impl.dart
@@ -0,0 +1,44 @@
+import 'package:image_picker/image_picker.dart';
+
+import 'camera_gallery_service.dart';
+
+class CameraGalleryServiceImpl implements CameraGalleryService {
+
+  final ImagePicker picker = ImagePicker();
+
+  @override
+  Future<String?> selectPhoto() async{
+    
+     // Pick an image.
+    final XFile? photo = await picker.pickImage(
+      source: ImageSource.gallery,
+      imageQuality: 80,
+    );
+
+    if (photo == null) return null;
+
+    print(photo.path);
+
+    return photo.path;
+
+  }
+
+  @override
+  Future<String?> takePhoto() async{
+   
+    // Capture a photo.
+    final XFile? photo = await picker.pickImage(
+      source: ImageSource.camera,
+      imageQuality: 80,
+      preferredCameraDevice: CameraDevice.rear,
+    );
+
+    if (photo == null) return null;
+
+    print(photo.path);
+
+    return photo.path;
+
+  }
+  
+}
