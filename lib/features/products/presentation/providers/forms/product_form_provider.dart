334c000711384c229243092f940a2dd373b234ba
Helmuth Sodetz <sodhel2001@gmail.com>
2023-09-27 12:39:48 -0500

    Mostrando imagen desde path abosulto

--- lib/features/products/presentation/providers/forms/product_form_provider.dart
+++ lib/features/products/presentation/providers/forms/product_form_provider.dart
@@ -119,6 +119,13 @@ class ProductFormNotifier extends StateNotifier<ProductFormState> {
     );
   }
 
+  void updateProductImage(String path) {
+    state = state.copyWith(
+      images: [...state.images, path],
+    );
+
+  }
+
   void onTitleChanged(String value) {
     state = state.copyWith(
         title: Title.dirty(value),
--- lib/features/products/presentation/screens/product_screen.dart
+++ lib/features/products/presentation/screens/product_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
 import 'package:teslo_shop/features/products/domain/domain.dart';
@@ -37,7 +39,7 @@ class ProductScreen extends ConsumerWidget {
 
                 if (photoPath == null) return;
 
-                photoPath;
+                ref.read(productFormProvider(productState.product!).notifier).updateProductImage(photoPath);
 
               }, 
               icon: const Icon(Icons.photo_library_outlined),
@@ -50,8 +52,8 @@ class ProductScreen extends ConsumerWidget {
 
                 if (photoPath == null) return;
 
-                photoPath;
-                
+                ref.read(productFormProvider(productState.product!).notifier).updateProductImage(photoPath);
+                                
               }, 
               icon: const Icon(Icons.camera_alt_outlined),
             ),
@@ -277,21 +279,41 @@ class _ImageGallery extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    if ( images.isEmpty ) {
+      return ClipRRect(
+        borderRadius: const BorderRadius.all(Radius.circular(20)),
+        child: Image.asset('assets/images/no-image.jpg', fit: BoxFit.cover )
+      );
+    }
+
+
     return PageView(
       scrollDirection: Axis.horizontal,
       controller: PageController(
         viewportFraction: 0.7
       ),
-      children: images.isEmpty
-        ? [ ClipRRect(
-            borderRadius: const BorderRadius.all(Radius.circular(20)),
-            child: Image.asset('assets/images/no-image.jpg', fit: BoxFit.cover )) 
-        ]
-        : images.map((e){
-          return ClipRRect(
-            borderRadius: const BorderRadius.all(Radius.circular(20)),
-            child: Image.network(e, fit: BoxFit.cover,),
+      children: images.map((image){
+          
+        late ImageProvider imageProvider;
+        if ( image.startsWith('http') ) {
+          imageProvider = NetworkImage(image);
+        } else {
+          imageProvider = FileImage( File(image) );
+        }
+
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(20)),
+              child: FadeInImage(
+                fit: BoxFit.cover,
+                image: imageProvider,
+                placeholder: const AssetImage('assets/loaders/bottle-loader.gif'),
+              )
+            ),
           );
+
+
       }).toList(),
     );
   }
